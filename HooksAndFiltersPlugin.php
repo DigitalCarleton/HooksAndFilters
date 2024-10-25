2f60d9dc699573051bac1417ae599700f11cd47d
apjmason <amason@carleton.edu>
2024-10-25 15:08:16 +0000

    add public collections show

--- HooksAndFiltersPlugin.php
+++ HooksAndFiltersPlugin.php
@@ -2,7 +2,7 @@
 
 class HooksAndFiltersPlugin extends Omeka_Plugin_AbstractPlugin
 {
-    public $_hooks = array('public_items_show');
+    public $_hooks = array('public_items_show','public_collections_show');
     
     public $_filters = array('public_navigation_main');
     
@@ -21,6 +21,21 @@ class HooksAndFiltersPlugin extends Omeka_Plugin_AbstractPlugin
         */
     }
     
+    public function hookPublicCollectionsShow($args)
+    {
+        echo "<p>LOOK! I'VE ADDED COLLECTION SPECIFIC CONTENT!!!!</p>";
+        
+        //we'll use this after we've looked at the Models example plugin
+        /*
+          $tweet1 = get_db()->getTable('Tweet')->find(1);
+          echo "<p>" . metadata($tweet1, 'text') . "</p>";
+          
+          $tweetId13 = get_db()->getTable('Tweet')->findTweetId(13);
+          echo "<p>" . metadata($tweetId13, 'text') . "</p>";          
+          
+        */
+    }
+
     public function filterPublicNavigationMain($navArray)
     {
         $navArray['Example'] = array('label' => 'Example', 'uri'=>'http://example.com');
